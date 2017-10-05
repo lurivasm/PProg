bf3a6718b9b0229819b224748878c245af7191e8
lurivasm <lucia_rm98@hotmail.com>
2017-10-05 22:22:44 +0200

    Actualizacion object.c y object.txt

--- V 0.001/object.c
+++ V 0.001/object.c
@@ -1,11 +1,13 @@
 #include "object.h"
 
 typedef struct {
-  Type t;                 //Type of object
-  int recurse[3];         //
-  int take;               //Can it be taken? 0 = NO, 1 = YES
-  int use;                //Can we use it? 0 = NO, 1 = YES
+  Type t;                 /*Type of object*/
+  int take;                /*Can it be taken? 0 = NO, 1 = YES*/
+  int use;                 /*Can we use it? 0 = NO, 1 = YES*/
+  int resourse[2];         /*Resourse[0] = -1(decrease)/ 0(nothing)/ 1(increase)*/
+                           /*Resourse[1] = Quantity*/
 }Object;
+
 
 Object *object_ini(){
   Object *o;
@@ -15,8 +17,8 @@ Object *object_ini(){
 
   o->take = ERROR;
   o->t = ERROR;
-  for(int i = 0; i < 3; i++){
-    o->recurse[i] = ERROR;
+  for(int i = 0; i < 2; i++){
+    o->resourse[i] = ERROR;
   }
   return o;
 }
@@ -29,13 +31,13 @@ void object_destroy(Object *o){
 }
 
 
-Object *object_set(Object *o, Type t, int resource[3], int take, int use){
+Object *object_set(Object *o, Type t, int resource[2], int take, int use){
   if(!o || t == ERROR || (take != 1 && take != 0) || (use != 0 && use != 1)) return NULL;
 
   o->take = take;
   o->t = t;
   o->use = use;
-  for(int i = 0; i < 3; i++){
+  for(int i = 0; i < 2; i++){
     o->resource[i] = resource[i];
   }
   return o;
