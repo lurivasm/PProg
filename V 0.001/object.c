c3fd239f90c99e633679fc33f91f8a090634d30d
Lucía Rivas <lucia_rm98@hotmail.com>
2017-10-19 19:44:33 +0200

    Mejorado

--- V 0.001/object.c
+++ V 0.001/object.c
@@ -4,8 +4,7 @@
   Type t;                 /*Type of object*/
   int take;                /*Can it be taken? 0 = NO, 1 = YES*/
   int use;                 /*Can we use it? 0 = NO, 1 = YES*/
-  int resource[2];         /*Resourse[0] = -1(decrease)/ 0(nothing)/ 1(increase)*/
-                           /*Resourse[1] = Quantity*/
+  int quantity;         /*-x(decrease)/ 0(nothing)/ 1(increase)*/
 };
 
 
@@ -18,9 +17,7 @@ Object *object_ini(void){
 
   o->take = ERROR;
   o->t = ERROR;
-  for(i = 0; i < 2; i++){
-    o->resource[i] = ERROR;
-  }
+  o->quantity = ERROR;
   return o;
 }
 
@@ -32,33 +29,32 @@ void object_destroy(Object *o){
 }
 
 
-Object *object_set(Object *o, Type t, int r0, int r1, int take, int use){
-  if(!o || t == ERROR || (take != 1 && take != 0) || (use != 0 && use != 1)) return NULL;
+Object *object_set(Object *o, Type t, int q, int take, int use){
+  if(!o || t == ERROR || (take != 1 && take != 0) || (use != 0 && use != 1) || q == ERROR) return NULL;
 
   int i;
 
   o->take = take;
   o->t = t;
   o->use = use;
-  o->resource[0]= r0;
-  o->resource[1]= r1;
+  o->quantity = q;
   return o;
 }
 
 
 Object *object_read(Object *o, char *name, int key){
   FILE *file = NULL;
-  int key_aux, type_aux, r0_aux, r1_aux, take_aux, use_aux;
+  int key_aux, type_aux, q_aux, take_aux, use_aux;
 
   file = fopen(name, "r");
   if(!file) return NULL;
 
   while(!feof(file)){
-    fscanf(file, "%d %d %d %d %d %d", &key_aux, &type_aux, &r0_aux, &r1_aux, &take_aux, &use_aux);
+    fscanf(file, "%d %d %d %d %d", &key_aux, &type_aux, &q_aux, &take_aux, &use_aux);
     if(key_aux == key) break;
   }
 
-  o = object_set(o, type_aux, r0_aux, r1_aux, take_aux, use_aux);
+  o = object_set(o, type_aux, q_aux, take_aux, use_aux);
   if(!o) return NULL;
 
   return o;
@@ -83,13 +79,8 @@ int object_take(Object *o){
 }
 
 
-int object_indecrease(Object *o){
-  if(!o) return ERROR;
-  return o->resource[0];
-}
-
-
 int object_quantity(Object *o){
   if(!o) return ERROR;
-  return o->resource[1];
+  return o->quantity;
 }
+
