c50af1e2bd440050d1b1dd125d54a28866efb4d9
Savagelaoch <juveiz2931@gmail.com>
2017-12-14 19:57:37 +0100

    mane supuestamente funcionando

--- Mane is faster than you/interface.c
+++ Mane is faster than you/interface.c
@@ -1,15 +1,26 @@
 #include "interface.h"
 
 
-Interface * inter_create(int rows,int columns){
+
+
+
+Interface * inter_create(int rows,int columns,int rb,int cb,int rs,int cs,int rt,int ct){
 	if (rows<=0||columns<=0) return NULL;
 	Interface* i;
 	i=(Interface*)malloc(sizeof(Interface));
 	if (i==NULL) return NULL;
+	i->rb = rb;
+	i->cb = cb;
+	i->rs = rs;
+	i->cs = cs;
+	i->rt = rt;
+	i->ct = ct;
 	i->rows=rows;
 	i->columns=columns;
-	i->board=win_new(1,1,rows,columns,BACKGROUND,FOREGROUND);
-	i->player='0';
+	i->board = win_new(rb,cb,22,86,BACKGROUND,FOREGROUND);
+	i->score = win_new(rs,cs,33,32,BACKGROUND,FOREGROUND);
+	i->text = win_new(rt,ct,11,86,BACKGROUND,FOREGROUND);
+	i->player=' ';
 	i->player_row=0;
 	i->player_column=0;
 	fprintf(stdout, "%c[2J", 27);
@@ -22,7 +33,12 @@ void inter_delete(Interface*i){
 		free(i);
 		return;
 	}
+	if (i->score==NULL) {
+		free(i);
+		return;
+	}
 	win_delete(i->board);
+	win_delete(i->score);
 	free(i);
 	return;
 }
@@ -38,9 +54,25 @@ int set_player(Interface* i,char player,int row,int column){
 
 int set_board(Interface* i,char **board,int row,int columns){
 	if (board==NULL||row>i->rows||columns>i->columns) return 0;
-	i->map=board;
-	i->map_rows=row;
-	i->map_columns=columns;
+	i->mapb=board;
+	i->mapb_rows=row;
+	i->mapb_columns=columns;
+	return 1;
+}
+
+int set_score(Interface* i,char **score,int row,int columns){
+	if (score==NULL||row>i->rows||columns>i->columns) return 0;
+	i->maps=score;
+	i->maps_rows=row;
+	i->maps_columns=columns;
+	return 1;
+}
+
+int set_text(Interface* i,char **text,int row,int columns){
+	if (text==NULL||row>i->rows||columns>i->columns) return 0;
+	i->mapt=text;
+	i->mapt_rows=row;
+	i->mapt_columns=columns;
 	return 1;
 }
 
@@ -48,29 +80,47 @@ int draw_board(Interface* i, int clear){
 	if (i==NULL) return 0;
 	if (clear) win_cls(i->board);
 	int j;
-	for (j=0;j<i->map_rows;j++){
-		win_write_line_at(i->board,j,0,i->map[j]);
+	for (j=0;j<i->mapb_rows;j++){
+		win_write_line_at(i->board,j,0,i->mapb[j]);
 	}
 	win_write_char_at(i->board,i->player_row,i->player_column,i->player);
+	return 1;
+}
+
+int draw_score(Interface* i, int clear){
+	if (i==NULL) return 0;
+	if (clear) win_cls(i->score);
+	int j;
+	for (j=0;j<i->maps_rows;j++){
+		win_write_line_at(i->score,j,0,i->maps[j]);
+	}
+
+	return 1;
+}
+
+
+int draw_text(Interface* i, int clear){
+	if (i==NULL) return 0;
+	if (clear) win_cls(i->text);
+	int j;
+	for (j=0;j<i->mapt_rows;j++){
+		win_write_line_at(i->text,j,0,i->mapt[j]);
+	}
+
 	return 1;
 }
 
 static int Dr[5] = {-1, 1, 0, 0, 0};
 static int Dc[5] = {0, 0, 1, -1, 0};
 int move(Interface *i,int direction){
-	if (i==NULL||direction>4||direction<0) return -1;
+	if (i==NULL||direction>4||direction<0) return -3;
 	int r,c;
 	r=i->player_row+Dr[direction];
 	c=i->player_column+Dc[direction];
-	if(r<0||c<0||r>=i->rows||c>=i->columns) return -1;
-	if (i->map[r][c]=='*') {
-		return -2;
-	}
-	
-	if (i->map[r][c]!=' ') {
+	if(r<0||c<0||r>=i->rows||c>=i->columns) return -2;
+	if (i->mapb[r][c]!=' ') {
 		return -1;
 	}
-	
 	win_write_char_at(i->board,i->player_row,i->player_column,' ');
 	i->player_column=c;
 	i->player_row=r;
@@ -133,4 +183,21 @@ char **create_map(char *file,int *size){
 	size[0]=rows;
 	size[1]=cols;
 	return map;
+}
+
+
+
+sc_rectangle* get_board(Interface *i){
+	if(!i) return NULL;
+	return i->board;
+}
+
+sc_rectangle* get_score(Interface *i){
+	if(!i) return NULL;
+	return i->score;
+}
+
+sc_rectangle* get_text(Interface *i){
+	if(!i) return NULL;
+	return i->text;
 }
--- Mane is faster than you/mane.c
+++ Mane is faster than you/mane.c
@@ -4,6 +4,8 @@
 
 struct termios initial;
 
+int flag=0;
+
 void _term_init() {
 	struct termios new;	          /*a termios structure contains a set of attributes about
 					  how the terminal scans and outputs data*/
@@ -37,42 +39,112 @@ void ck_count(int max){
 		time_t instant;
 		instant=time(NULL);
 		if (instant-itime>max){
+			flag=1;
 			return;
 		}
 	}
 }
-int mane(char* file){
+
+int move_mane(Interface *i,int direction){
+	if (i==NULL||direction>4||direction<0) return -3;
+	int r,c;
+	r=i->player_row+Dr[direction];
+	c=i->player_column+Dc[direction];
+	if(r<0||c<0||r>=i->rows||c>=i->columns) return -2;
+	if (i->mapb[r][c]!=' ') {
+		return -1;
+	}
+  if (i->mapb[r][c] == '*'){
+		return -2;
+	}
+	if (i->mapb[r][c] == 'M'){
+		return 1;
+	}
+	win_write_char_at(i->board,i->player_row,i->player_column,' ');
+	i->player_column=c;
+	i->player_row=r;
+	win_write_char_at(i->board,i->player_row,i->player_column,i->player);
+	return r<<8 +c;
+}
+int mane(char* file; Interface* i){
 	if (file==NULL) return 1;
-	Interface *maze;
-	int size [2];
-	int flag=0,mover;
-	char **map;
-	map=create_map(file,size);
-	maze=inter_create(size[0],size[1]);
-	set_player(maze,'J',23,22);
-	set_board(maze,map,size[0],size[1]);
-	draw_board(maze,1);
+	int sizeb[2],sizes[2],sizet[2];
+	int mover;
+	char** board;
+	char** score;
+	char** text;
+	board = create_map(file,sizeb);
+	score = create_map("score",sizes);
+	text = create_map("text",sizet);
+	set_board(i,board,sizeb[0],sizeb[1]);
+	draw_board(i,1);
+
+	set_score(i,score,sizes[0],sizes[1]);
+	draw_score(i,1);
+
+	set_text(i,text,sizet[0],sizet[1]);
+	draw_text(i,1);
+
+	set_player(i,'J',23,44);
+
+	free(board);
+	free(score);
+	free(text);
+
 	_term_init();
-	while (flag!=-2){
+	flag=0;
+	while (flag == 0){
 		mover=_read_key();
 		fflush(stdout);
 
-		if (mover == 'q') {
+		if (mover == '1') {
       		tcsetattr(fileno(stdin), TCSANOW, &initial);	/*We now restore the settings we back-up'd
 							  so that the termial behaves normally when
 							  the program ends */
-      		return 0;
+      		return 3;
     	}
-    	flag=move(maze,-mover);
-    	
+    mover=move_mane(maze,-mover);
+		if (mover == '-2') {
+						tcsetattr(fileno(stdin), TCSANOW, &initial);	/*We now restore the settings we back-up'd
+								  so that the termial behaves normally when
+								  the program ends */
+	      		return 0;
+	    }
+		if (mover == '1') {
+							tcsetattr(fileno(stdin), TCSANOW, &initial);	/*We now restore the settings we back-up'd
+									  so that the termial behaves normally when
+									  the program ends */
+		      		return 1;
+		    }
+
 		fflush(stdout);
 	}
-	map=create_map("lose.txt",size);
-	set_board(maze,map,size[0],size[1]);
-	draw_board(maze,1);
+
 
 	tcsetattr(fileno(stdin), TCSANOW, &initial);	/*We now restore the settings we back-up'd
 							  so that the termial behaves normally when
 							  the program ends */
-      		return 1;
+      		return 0;
+}
+
+int play_mane(){
+	Interface *i;
+	int done=0,fail=0,res;
+	pthread_t pth;
+	char map;
+	i=inter_create(33,114,0,0,0,86,22,0);
+	if (i==NULL) return -1;
+	while (done<6 || fail<3){
+		pthread_create(&pth, NULL, ck_count, 20);
+		res = mane(map,i);
+		if (res == 3){
+			return 1;
+		}
+		if (res == 1){
+			done ++;
+		}
+		else if (res == 0){
+			fail++;
+		}
+	}
 }
