fbe7e6cb5a42239a54f1275c5974251dcceaafb5
danist7 <daniel.santo-tomas@estudiante.uam.es>
2017-12-18 18:19:38 +0100

    Actualización,he metido term init en interface.c

--- Blackjack/train.c
+++ Blackjack/train.c
@@ -11,38 +11,16 @@
 #include <pthread.h>
 #include "interface.h"
 
-void _term_init() {
-	struct termios new;	          /*a termios structure contains a set of attributes about
-					  how the terminal scans and outputs data*/
+struct termios initial;
 
-	tcgetattr(fileno(stdin), &initial);    /*first we get the current settings of out
-						 terminal (fileno returns the file descriptor
-						 of stdin) and save them in initial. We'd better
-						 restore them later on*/
-	new = initial;	                      /*then we copy them into another one, as we aren't going
-						to change ALL the values. We'll keep the rest the same */
-	new.c_lflag &= ~ICANON;	              /*here we are setting up new. This line tells to stop the
-						canonical mode (which means waiting for the user to press
-						enter before sending)*/
-	new.c_lflag &= ~ECHO;                 /*by deactivating echo, we tell the terminal NOT TO
-						show the characters the user is pressing*/
-	new.c_cc[VMIN] = 1;                  /*this states the minimum number of characters we have
-					       to receive before sending is 1 (it means we won't wait
-					       for the user to press 2,3... letters)*/
-	new.c_cc[VTIME] = 0;	              /*I really have no clue what this does, it must be somewhere in the book tho*/
-	new.c_lflag &= ~ISIG;                 /*here we discard signals: the program won't end even if we
-						press Ctrl+C or we tell it to finish*/
 
-	tcsetattr(fileno(stdin), TCSANOW, &new);  /*now we SET the attributes stored in new to the
-						    terminal. TCSANOW tells the program not to wait
-						    before making this change*/
-}
+
 
 void main(){
   Interface* i;
   FILE *f;
-  int quit;
-  int j=0;
+  int m;
+  int j=0,k;
   char** board;
   char** score;
   char** text;
@@ -50,11 +28,14 @@ void main(){
 
 
   /*We create the maps for the board,score and text and set them on the interface*/
-  board = create_map("",sizeb);
+  board = create_map("tr",sizeb);
   score = create_map("score",sizes);
   text = create_map("text",sizet);
 
   i=inter_create(33,114,0,0,0,86,22,0);
+
+  set_player(i,'D',5,6);
+	printf("\e[?25l");
 
   set_board(i,board,sizeb[0],sizeb[1]);
   draw_board(i,1);
@@ -65,8 +46,38 @@ void main(){
   set_text(i,text,sizet[0],sizet[1]);
   draw_text(i,1);
 
+  set_player(i,'D',5,6);
+
   _term_init();
 
+  while(1){
+	m = _read_key();
+	/*pressing q it exits*/
+	if (m == 'q') {
+		inter_delete(i);
+    tcsetattr(fileno(stdin), TCSANOW, &initial);	/*We now restore the settings we back-up'd
+							  so that the termial behaves normally when
+							  the program ends */
+    return;
+  }
+  move(i,-m);
+	}
 
+  tcsetattr(fileno(stdin), TCSANOW, &initial);
+  for(k = 0;k<sizeb[0];k++){
+		free(board[k]);
+	}
+	for(k = 0;k<sizet[0];k++){
+		free(text[k]);
+	}
+	for(k = 0;k<sizes[0];k++){
+		free(score[k]);
+	}
 
+	free(board);
+	free(text);
+	free(score);
+	tcsetattr(fileno(stdin), TCSANOW, &initial);	/*We now restore the settings we back-up'd
+							so that the termial behaves normally when
+							the program ends*/
 }
